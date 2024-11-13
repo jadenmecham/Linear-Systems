136ff33deb9adfc903426a1a52929146b2810218
Jaden Mecham <112149558+jadenmecham@users.noreply.github.com>
2024-11-13 12:07:07 -0800

    Update HW5.m

--- Homework/HW5.m
+++ Homework/HW5.m
@@ -4,5 +4,12 @@ B=[1; 1];
 C=[1 1];
 D=[0];
 
-contMat = [B A*B]
-rank = rank(contMat)
+contMat = [B A*B];
+obsMat = [C; C*A];
+
+Cext = [1 0; 3 1];
+Tc = inv(Cext);
+
+Atilde = Tc*A*inv(Tc);
+Btilde = Tc*B;
+Ctilde = C*inv(Tc)
