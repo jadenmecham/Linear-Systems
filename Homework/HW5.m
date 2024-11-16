8d29d21cb1c29e55d905323ca64c530f98f2e732
Jaden Mecham <112149558+jadenmecham@users.noreply.github.com>
2024-11-15 16:38:42 -0800

    Update HW5.m

--- Homework/HW5.m
+++ Homework/HW5.m
@@ -7,7 +7,7 @@ D=[0];
 contMat = [B A*B];
 obsMat = [C; C*A];
 
-Cext = [1 0; 3 1];
+Cext = [1 0; 1 1];
 Tc = inv(Cext);
 
 Atilde = Tc*A*inv(Tc);
@@ -15,19 +15,19 @@ Btilde = Tc*B;
 Ctilde = C*inv(Tc);
 
 Oext = [1 1; 0 1];
-Tc2 = inv(Oext);
+Tc2 = inv(Oext)
 Atilde2 = Tc2*A*inv(Tc2)
 Btilde2 = Tc2*B
 Ctilde2 = C*inv(Tc2)
 %% 
 % Problem 5.2
-syms a b c d q1 q2
+syms a b c q1 q2
 A = [0 1; -2 -3];
 AT = [0 -2; 1 -3];
-P = [a b; c d];
+P = [a b; b c];
 Q= [q1 0; 0 q2];
 
 eq = AT*P + P*A
-lyap(A,Q)
+
 
 
