8ac112a191e067b7b22bcf8bf02ef664e17799ac
Jaden Mecham <112149558+jadenmecham@users.noreply.github.com>
2024-11-19 14:43:42 -0800

    Update HW5.m

--- Homework/HW5.m
+++ Homework/HW5.m
@@ -1,24 +1,35 @@
 % Problem 5.1
-A=[-1 4; 4 -1];
-B=[1; 1];
-C=[1 1];
+A=[1 1 0; 0 1 0; 0 1 1];
+B=[0;1;0];
+C=[1 3 1];
 D=[0];
 
-contMat = [B A*B];
-obsMat = [C; C*A];
+contMat = [B A*B A^2*B];
 
-Cext = [1 0; 1 1];
+
+% Controllable Subsys
+Cext = [0 1 0; 1 1 0; 0 1 1];
 Tc = inv(Cext);
 
 Atilde = Tc*A*inv(Tc);
 Btilde = Tc*B;
 Ctilde = C*inv(Tc);
 
-Oext = [1 1; 0 1];
-Tc2 = inv(Oext)
-Atilde2 = Tc2*A*inv(Tc2)
-Btilde2 = Tc2*B
-Ctilde2 = C*inv(Tc2)
+% Observable Subsys
+obsMat = [C; C*A; C*A^2]
+Oext = [1 3 1; 1 5 1; 0 0 1]
+Tc2 = Oext;
+Atilde2 = Tc2*A*inv(Tc2);
+Btilde2 = Tc2*B;
+Ctilde2 = C*inv(Tc2);
+
+% Both
+Oboth = [Ctilde; Ctilde*Atilde; Ctilde*Atilde^2];
+Obothext = [3 5 1; 5 7 1; 0 0 1];
+Tcboth = Obothext;
+Atildeboth = Tcboth*Atilde*inv(Tcboth)
+Btildeboth = Tcboth*Btilde
+Ctildeboth = Ctilde*inv(Tcboth)
 %% 
 % Problem 5.2
 syms a b c q1 q2
