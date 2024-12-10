bad7e4770cdb35640e0bbf7b4e904f167e8d0085
Jaden Mecham <112149558+jadenmecham@users.noreply.github.com>
2024-12-09 16:23:58 -0800

    project

--- Final Project/problem1.m
+++ Final Project/problem1.m
@@ -87,8 +87,8 @@ C = [1 0 0;
 D = [0 0; 
      0 0];
 
-contMat = [B A*B A^2*B]; % controllability matrix
-obsMat = [C; C*A; C*A^2]; % observability matrix
+contMat = [B A*B A^2*B] % controllability matrix
+obsMat = [C; C*A; C*A^2] % observability matrix
 
 disp("rank of controllability matrix:")
 disp(rank(contMat));
@@ -106,48 +106,16 @@ A = [0    1    0;
  
 B = [0;
      0;
-     1];
+     2];
  
-C = [2  0  0];
+C = [1  0  0];
 
 D = [0];
 
-contMat = [B A*B A^2*B]; % controllability matrix
-obsMat = [C; C*A; C*A^2]; % observability matrix
+contMat = [B A*B A^2*B] % controllability matrix
+obsMat = [C; C*A; C*A^2] % observability matrix
 
 disp("rank of controllability matrix:")
 disp(rank(contMat));
 disp("rank of observability matrix:")
 disp(rank(obsMat));
-%%
-% problem 1f
-
-% known properties:
-L = 1;  % inductance 
-R = 2;  % resistance 
-J = 1;  % moment of inertia
-b = 1;  % viscous damping  
-K = 2;  % torque constant
-
-% state space matrices, ignoring u2 and y2:
-A = [0    1    0;
-     0   -b/J  K/J;
-     0   -K/L -R/L];
-
-B = [0;
-     0;
-     1/L];
- 
-C = [2 0 0];
-
-D = [0];
-
-% make the state space modelm
-sys_ss = ss(A, B, C, D);
-
-% find transfer function
-sys_tf = tf(sys_ss);
-
-% show transfer function
-disp('transfer function G(s):');
-sys_tf
